3666783926dcfba5c1fc3162469a1580230c03a2
notnotjake <jake@notnotjake.com>
2023-11-18 20:39:31 -0500

    Initial work on index

--- .eleventy.js
+++ .eleventy.js
@@ -11,6 +11,14 @@ module.exports = function(eleventyConfig) {
 		"source/_assets/js": "js"
 	})
 	
+	/* SHORTCODE: Static Assets Linking
+	~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+	eleventyConfig.addShortcode("asset", function ( path ) {
+		// Would be better if we could read the url from the /_data/site.json file
+		const host = 'https://assets.artfellowship.space'
+		return host + path
+	})	
+	
 	/* Set input/output
 	~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
 	return {
