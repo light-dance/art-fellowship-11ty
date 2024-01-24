45e6f27f843934a832b6f1c0e8ef0457f2ef6b8b
notnotjake <jake@notnotjake.com>
2024-01-24 17:14:02 -0500

    home signup modal esc key to dismiss

--- source/_assets/js/index.js
+++ source/_assets/js/index.js
@@ -9,17 +9,16 @@ document.querySelector('#nav-btn').addEventListener('click', () => {
 	}
 })
 
-// Handle get-started buttons
-document.querySelectorAll('.get-started-btn').forEach( i => {
-	i.addEventListener('click', (e) => {
-		e.preventDefault()
-		document.querySelector('#signup').classList.remove('hide')
-		document.querySelector('#signup').classList.add('show')
-	})
-})
 
-// handle hiding the overlay
-document.querySelector('#signup div.overlay').addEventListener('click', () => {
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+Sign-up Modal Interactions
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+function showSignup () {
+	const m = document.querySelector('#signup')
+	m.classList.remove('hide')
+	m.classList.add('show')
+}
+function hideSignup () {
 	const m = document.querySelector('#signup')
 	if (! m.classList.contains('hide')) {
 		m.classList.add('close')
@@ -29,7 +28,28 @@ document.querySelector('#signup div.overlay').addEventListener('click', () => {
 			m.classList.add('hide')
 		}, 500)
 	}
+}
+
+// Hide signup when ESC key
+document.addEventListener('keydown', ( e ) => {
+	if ( e.key === 'Escape' && !document.querySelector('#signup').classList.contains('hide') ) {
+		hideSignup()
+	}
 })
+
+// Show Signup when link is clicked
+document.querySelectorAll('.get-started-btn').forEach( i => {
+	i.addEventListener('click', (e) => {
+		e.preventDefault()
+		showSignup()
+	})
+})
+
+// Hide signup when you click elsewhere
+document.querySelector('#signup div.overlay').addEventListener('click', () => {
+	hideSignup()
+})
+/* [END] Sign-up Modal Interactions */
 
 // switch between testimonials
 const responsesSet = document.querySelectorAll('ul.responses li')
