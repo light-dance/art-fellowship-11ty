d7938b39c4a6d630595fb4b2f801e0781d7ebc8d
notnotjake <jake@notnotjake.com>
2024-01-26 15:55:42 -0500

    changes article animation to single p elem
    simpler and removes the animation lag when scrolling quickly

--- source/_assets/js/index-animations.js
+++ source/_assets/js/index-animations.js
@@ -2,28 +2,9 @@ gsap.registerPlugin(ScrollTrigger)
 
 /* Animate ARTICLE text on scroll
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
-let paragraph1 = new SplitText(document.querySelector('#article-p1'), {type: "chars,words"})
-let paragraphHighlight = gsap.timeline({
-	scrollTrigger: {
-		trigger: '#article-p1',
-		start: 'top 60%',
-		end: 'bottom 60%',
-		scrub: 1.5,
-	}
-})
-paragraphHighlight.fromTo(paragraph1.chars, {
-	opacity: 0.5
-},
+let article = new SplitText(document.querySelector('#problem-statement'), {type: "chars,words"})
+gsap.fromTo(article.chars, 
 {
-	opacity: 1,
-	color: 'hsl(0, 0%, 0%)',
-	duration: 1,
-	ease: 'power3',
-	stagger: 0.05
-})
-
-let paragraph2 = new SplitText(document.querySelector('#article-p2'), {type: "chars,words"})
-gsap.fromTo(paragraph2.chars, {
 	opacity: 0.5
 },
 {
@@ -33,26 +14,10 @@ gsap.fromTo(paragraph2.chars, {
 	ease: 'power3',
 	stagger: 0.05,
 	scrollTrigger: {
-		trigger: '#article-p2',
+		trigger: '#problem-statement',
 		start: 'top 60%',
 		end: 'bottom 60%',
-		scrub: 1.5
+		scrub: 2
 	}
-})
-let paragraph3 = new SplitText(document.querySelector('#article-p3'), {type: "chars,words"})
-gsap.fromTo(paragraph3.chars, {
-	opacity: 0.5
-},
-{
-	opacity: 1,
-	color: 'hsl(0, 0%, 0%)',
-	duration: 1,
-	ease: 'power3',
-	stagger: 0.05,
-	scrollTrigger: {
-		trigger: '#article-p3',
-		start: 'top 60%',
-		end: 'bottom 60%',
-		scrub: 1.5
-	}
-})
+}
+)
